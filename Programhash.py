dae17ae8103ea877fa2185d6682f39828d874698
Manabu Tokunaga <manabutokunaga@manabu-imac.local>
2010-12-18 16:39:58 -0800

    Added comment in Programhash.py

--- Programhash.py
+++ Programhash.py
@@ -1,3 +1,7 @@
+# Example to collect files from Directory then compute the hash
+# then print the hex digest of the file along with the file name
+# It is written for Windows program files but can be used for
+# any directory.
 import hashlib
 import os
 serverDir = "c:\\program files\\someprogram"
